1c70de929087c5717a463233ebe7502aaac4ba36
Parakh Singhal <parakh@thinkingcog.com>
2021-07-07 19:05:35 +0530

    Changed the release-nuget.yml file to pass in the correct location of the project

---
+++ DALHelper.MSTests/DBDataHelper.MSTests.Integrated.Methods.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DALHelper.MSTests
+{
+    [TestClass]
+    public class DBDataHelperMSTestsIntegratedMethods: DBDataHelperMSTestsUnitBase
+    {
+        //ExecSQL
+        //GetRowsAffected
+        //GetDataReader
+        //GetDataTable
+        //GetDataSet
+
+        //CRUD for all methods 
+        //With and without sql parameters
+        //Queries and stored procedures
+        //Total of 4*2*2 = 16 tests per method
+    }
+}
---
+++ DALHelper.MSTests/DBDataHelper.MSTests.Unit.Methods.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ThinkingCog.DALHelper.SQLServer;
+
+/*
+ * This class is used to do unit testing of all the contracts defined in the DBDataHelper class.
+ * The class derives from the DALHelper.Tests.Base class and uses the setup and teardown methods
+ * defined in the base class.
+ */
+
+namespace DALHelper.MSTests
+{
+    [TestClass]
+    public class DBDataHelperMSTestsUnitMethods : DBDataHelperMSTestsUnitBase
+    {
+        #region Unit Tests Testing Contracts
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExecSQL_NullSQLStatementProvided_ThrowArgumentNullException()
+        {            
+            try
+            {
+                dbdatahelperObject.ExecSQL(null, SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExecSQL_ZeroLengthSQLStatementProvided_ThrowArgumentException()
+        {
+            try
+            {
+                dbdatahelperObject.ExecSQL("", SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetRowsAffected_NullSQLStatementProvided_ThrowArgumentNullException()
+        {
+            try
+            {
+                dbdatahelperObject.GetRowsAffected(null, SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetRowsAffected_ZeroLengthSQLStatementProvided_ThrowArgumentException()
+        {
+            try
+            {
+                dbdatahelperObject.GetRowsAffected("", SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataTable_NullSQLStatementProvided_ThrowArgumentNullException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataTable(null, SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataTable_ZeroLengthSQLStatementProvided_ThrowArgumentException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataTable("", SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataView_NullSQLStatementProvided_ThrowArgumentNullException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataView(null, SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataView_ZeroLengthSQLStatementProvided_ThrowArgumentException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataView("", SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataReader_NullSQLStatementProvided_ThrowArgumentNullException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataTable(null, SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataReader_ZeroLengthSQLStatementProvided_ThrowArgumentException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataTable("", SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataSet_NullSQLStatementProvided_ThrowArgumentNullException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataTable(null, SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDataSet_ZeroLengthSQLStatementProvided_ThrowArgumentException()
+        {
+            try
+            {
+                dbdatahelperObject.GetDataTable("", SQLTextType.Query);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
