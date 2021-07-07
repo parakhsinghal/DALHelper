8e1a66ed0b802d3e911310d5b47d7f8a2c803c36
Parakh Singhal <parakh@thinkingcog.com>
2021-07-07 19:13:36 +0530

    Changed the release-nuget.yml file to pass in the correct location of the project

---
+++ DALHelper/SQLTextType.cs
@@ -0,0 +1,19 @@
+﻿namespace ThinkingCog.DALHelper.SQLServer
+{
+    /// <summary>
+    /// The enum is used to suggest whether the passed sql text is a 
+    /// SQL query or a stored procedure.
+    /// </summary>
+    public enum SQLTextType
+    {
+        /// <summary>
+        /// Signifies that passed sql text is an ad hoc SQL query.
+        /// </summary>
+        Query=  0,
+
+        /// <summary>
+        /// Signifies that passed sql text is a stored procedure.
+        /// </summary>
+        Stored_Proc = 1
+    }
+}
